8a5c9a5f3bd8ac25178e0b2302c276bca74b0979
Sameerijaz112 <sameerejaz970@gmail.com>
2022-07-01 03:48:51 -0700

    commit

---
+++ if.js
@@ -0,0 +1,178 @@
+/* let student = {
+
+    Name:'Sameer',
+    Age:21,
+    Game:'Pubg',
+    Singer:'Youngstunner'
+
+}
+
+console.log(student); */
+/* let student = {
+
+    Name:'Sameer',
+    class:12,
+    Age:21,
+    Game:'pubg',
+}
+console.log(student); */
+/* let laptop = {
+    Name:'hp',
+    Model:'Folio 9470m',
+    Price:30000,
+}
+/* let processlaptop = (laptopobject) => {
+
+    if(laptopobject.price !== 31000 && laptopobject.price <31000) {
+        console.log('you are not Authorized  ' );
+        return;
+
+    }
+   
+}
+console.log(laptopobject); */
+/* console.log(laptop); */ 
+/* let Mobile = {
+    Name:'Tecno',
+    Model:'pova 2',
+    Price:30000,
+}
+let processmobile = (mobileobject) => {
+    if(mobileobject.Price !== 31000 && mobileobject.Price  <31000 );
+    console.log('You are not authorized');
+    return;
+    console.log(mobileobject);
+}
+
+processmobile(mobile) */
+/* let Mobile = {
+    Name:'Tecno' ,
+    Model:'pova2',
+    Price:30000,
+}
+let processmobile = (objectmobile) => {
+         console.log(objectmobile);
+
+}
+processmobile(Mobile) ; */
+
+
+/* let mobile = {
+    Name:'Tecno',
+    Model:'pova 2',
+    price:30000,
+}
+
+let processmobile = (objectmobile) => {
+      console.log(objectmobile) ;
+
+}
+processmobile(mobile) ; */
+/* let LCD = {
+    Name:'sonny',
+    Model:'dont know',
+    price:50000,
+}
+let processLCD = (objectLCD) => {
+    console.log(objectLCD);
+}
+processLCD(LCD) ; */
+/* let Fan = {
+    Name:'Sony',
+    color:'white',
+    price:15000,
+}
+let processfan = (objectfan) => {
+    if (objectfan.price !== 16000 && objectfan.price < 16000) {
+        console.log('you are not Authorized');
+        return;
+    }
+    console.log(objectfan);
+}
+processfan (Fan) ; */
+/* let Sameer = {
+    Name:'Sameer',
+    Age:21,
+    Class:'12th',
+}
+let processSameer = (objectSameer) => {
+    console.log(objectSameer);
+}
+processSameer(Sameer); */
+/* let Sameer = {
+    Name:'Sameer',
+    Class:'12th',
+    Age:'21',
+}
+let processSameer = (objectSameer) => {
+    console.log(objectSameer);
+}
+processSameer = (Sameer); */
+/* let Car = {
+ Name:'honda',
+ Price:200000, 
+}
+let processCar = (objectCar) => {
+   if (objectCar.Price !== 210000 && objectCar.Price <210000 ) {
+    console.log('this is not a car');
+    return;
+    
+   }
+   if (objectCar.Name !== honda  ) {
+    console.log( 'you are not Authorized');
+    
+   }
+   console.log(objectCar);
+
+}
+processCar(Car); */
+/* 
+let Bike = {
+    Name:'Suzuki',
+    Price:150000,
+    color:'red and black',
+
+}
+
+let processBike = (objectBike) => {
+    if (objectBike.Price !== 160000 && objectBike.Price  <160000) {
+        console.log('This bike is not Authorized');
+        return;
+        
+    }
+    console.log(objectBike);
+}
+processBike(Bike) ; */
+/* let AC = {
+    Name:'Orient',
+    Color:'white',
+    Price:75000,
+}
+let processAC = (objectiveAc) => {
+    console.log(objectiveAc);
+}
+processAC(AC); */
+let Bike = {
+    Name:'Honda',
+    Color:'Red',
+    Price:150000
+}
+let processBike =(objectBike) => {
+    if (objectBike.Price !== 16000000 && objectBike.Price <160000  ) {
+        console.log('this is not a Bike');
+        
+    }
+    if (objectBike.Name === 'Honda') {
+        console.log('This is a Honda Bike');
+        
+    }
+    if (objectBike.Color !== 'white') {
+
+       console.log('This is not a Red Bike');
+
+        
+    }
+    console.log(objectBike);
+}
+processBike(Bike);
+
